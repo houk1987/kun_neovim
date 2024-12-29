3efe20ebc249f4d12d3481da3395c055b69608d5
hk <hk@hkdeMacBook-Pro.lan>
2024-12-29 17:45:37 +0800

    add init.lua lua/options lua/keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,19 @@
+-- 定义mapleader 
+
+vim.g.mapleader=' '
+
+-- map 
+
+local map = vim.keymap.set
+
+-- 定义 insert model keymap
+
+map('i','jk','<esc>')
+
+-- 定义 normal model keymap
+
+map('n','<leader>w','<cmd>w<cr>')
+map('n','<leader>q','<cmd>q<cr>')
+map('n','<leader>s','<cmd>source %<cr>')
+map('n','<leader>n','<cmd>bnext<cr>')
+map('n','<leader>e','<cmd>Explore<cr>')
---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+-- 设置基本选项
+local options={
+	nu=true,
+	rnu=true,
+	ts=4,
+	sts=4,
+	sw=4
+}
+
+for k,v in pairs(options)do
+	vim.opt[k]=v
+end
